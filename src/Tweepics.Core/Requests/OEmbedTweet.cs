f50b3286ad27b4d08156dfdff84f5480a3fe2914
Nicholas J. Gravante <njgravante@gmail.com>
2018-09-14 10:15:40 -0400

    Add static class for retrieving oembed tweet data

---
+++ src/Tweepics.Core/Requests/OEmbedTweet.cs
@@ -0,0 +1,15 @@
+﻿using Tweetinvi.Models.DTO;
+
+namespace Tweepics.Core.Requests
+{
+    public static class OEmbedTweet
+    {
+        public static string GetHtml(string tweetUrl)
+        {
+            var oembed = Tweetinvi.TwitterAccessor.ExecuteGETQuery<IOEmbedTweetDTO>
+                        ($"https://publish.twitter.com/oembed?url={tweetUrl}&omit_script=true&hide_thread=true");
+
+            return oembed.HTML;
+        }
+    }
+}
