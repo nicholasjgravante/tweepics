890f7358257c1c08260e7989947952253afd4478
Nicholas J. Gravante <njgravante@gmail.com>
2018-09-19 13:09:43 -0400

    Replace name properties of PublicOfficial with Name type

---
+++ src/Tweepics.Core/Models/Name.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tweepics.Core.Models
+{
+    public class Name
+    {
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+        public string FirstLast { get; set; }
+        public string Full { get; set; }
+
+        public Name() { }
+
+        public Name(string first, string middle, string last)
+        {
+            First = first;
+            Middle = middle;
+            Last = last;
+            FirstLast = first + " " + last;
+            Full = first + " " + middle + " " + last;
+        }
+    }
+
+
+}
--- src/Tweepics.Core/Models/PublicOfficial.cs
+++ src/Tweepics.Core/Models/PublicOfficial.cs
@@ -5,10 +5,7 @@ namespace Tweepics.Core.Models
     public class PublicOfficial
     {
         public string TweepicsId { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string FullName { get; set; }
+        public Name Name { get; set; }
         public string State { get; set; }
         public string Party { get; set; }
         public long TwitterId { get; set; }
@@ -16,18 +13,14 @@ namespace Tweepics.Core.Models
 
         public PublicOfficial () { }
 
-        public PublicOfficial(string firstName, string middleName, string lastName, string state, 
-                              string party, long twitterId, string twitterScreenName)
-            : this ("", firstName, middleName, lastName, state, party, twitterId, twitterScreenName) { }
+        public PublicOfficial(Name name, string state, string party, long twitterId, string twitterScreenName)
+            : this ("", name, state, party, twitterId, twitterScreenName) { }
 
-        public PublicOfficial(string tweepicsId, string firstName, string middleName, string lastName, 
-                              string state, string party, long twitterId, string twitterScreenName)
+        public PublicOfficial(string tweepicsId, Name name, string state, string party, 
+                              long twitterId, string twitterScreenName)
         {
             TweepicsId = tweepicsId;
-            FirstName = firstName;
-            MiddleName = middleName;
-            LastName = lastName;
-            FullName = firstName + " " + middleName + " " + lastName;
+            Name = name;
             State = state;
             Party = party;
             TwitterId = twitterId;
