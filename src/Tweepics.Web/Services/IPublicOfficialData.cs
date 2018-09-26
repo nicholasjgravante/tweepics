1c4fb86748cc14eb359794fcfeb46b640fa4ccb3
Nicholas J. Gravante <njgravante@gmail.com>
2018-09-26 12:42:56 -0400

    Add service to retrieve information on public officials from database

---
+++ src/Tweepics.Web/Services/IPublicOfficialData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Tweepics.Core.Models;
+
+namespace Tweepics.Web.Services
+{
+    public interface IPublicOfficialData
+    {
+        List<PublicOfficial> GetAll();
+    }
+}
---
+++ src/Tweepics.Web/Services/PublicOfficialData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tweepics.Core.Database;
+using Tweepics.Core.Models;
+
+namespace Tweepics.Web.Services
+{
+    public class PublicOfficialData : IPublicOfficialData
+    {
+        private readonly IConfiguration _configuration;
+
+        public PublicOfficialData(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<PublicOfficial> GetAll()
+        {
+            string connectionString = _configuration["ConnectionString"];
+
+            PublicOfficialReader publicOfficialReader = new PublicOfficialReader(connectionString);
+            List<PublicOfficial> officials = new List<PublicOfficial>();
+            officials = publicOfficialReader.ReadFromDb();
+
+            return officials;
+        }
+    }
+}
