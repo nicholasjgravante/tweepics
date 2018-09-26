a2ae61023e2ac7b3444a0654ee2826b3f988f244
Nicholas J. Gravante <njgravante@gmail.com>
2018-09-26 12:56:56 -0400

    Add class to represent a public official and his/her tweets

---
+++ src/Tweepics.Core/Models/TweetsByOfficial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Tweepics.Core.Models
+{
+    public class TweetsByOfficial
+    {
+        public List<Tweet> Tweets { get; set; }
+        public PublicOfficial PublicOfficial { get; set; }
+
+        public TweetsByOfficial(List<Tweet> tweets, PublicOfficial official)
+        {
+            Tweets = tweets;
+            PublicOfficial = official;
+        }
+    }
+}
