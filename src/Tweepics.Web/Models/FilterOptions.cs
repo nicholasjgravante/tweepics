e692d529ac80e60cd86599837e7dd91a5465503e
Nicholas J. Gravante <njgravante@gmail.com>
2018-10-02 16:24:23 -0400

    Create class to represent options for filtering tweet results

---
+++ src/Tweepics.Web/Models/FilterOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Tweepics.Web.Services
+{
+    public class FilterOptions
+    {
+        public List<string> Officials { get; set; }
+        public List<string> States { get; set; }
+        public List<string> Parties { get; set; }
+    }
+}
