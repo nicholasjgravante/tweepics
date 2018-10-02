15b69f36e87b976bfc8d9b2bf1f603581d812802
Nicholas J. Gravante <njgravante@gmail.com>
2018-10-02 16:56:55 -0400

    Add models for requesting and displaying tweets

---
+++ src/Tweepics.Web/Models/TweetQueryInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tweepics.Web.Models
+{
+    public class TweetQueryInputModel
+    {
+        public string QueryType { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public string OriginalQuery { get; set; }
+
+        public int? Page { get; set; }
+        public List<string> Officials { get; set; }
+        public List<string> States { get; set; }
+        public List<string> Parties { get; set; }
+    }
+}
---
+++ src/Tweepics.Web/Models/TweetQueryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tweepics.Web.Services;
+
+namespace Tweepics.Web.Models
+{
+    public class TweetQueryModel
+    {
+        // "Search" (user searched from home page) or "Tag" (user selected one of the tag options)
+        public string QueryType { get; set; } 
+
+        // Search query or tag name
+        [MinLength(2), MaxLength(50)]
+        public string OriginalQuery { get; set; }
+
+        public int? Page { get; set; }
+        public FilterOptions FilterOptions { get; set; }
+    }
+}
